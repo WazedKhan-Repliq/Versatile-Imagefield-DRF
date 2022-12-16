7e3e4f4d52cc111154c0edd25a412583c25c7f18
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2022-12-16 22:38:01 +0600

    init

--- post/serializers.py
+++ post/serializers.py
@@ -1,35 +1,15 @@
 from rest_framework import serializers
 
-from versatileimagefield.serializers import VersatileImageFieldSerializer
-
 from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     """Serializes Person instances"""
-    image = VersatileImageFieldSerializer(
-        sizes=[
-            ('orgnizal_size', 'url'),
-            ('thumbnail', 'thumbnail__100x100'),
-            ('square_crop', 'crop__400x400'),
-            # ('small_square_crop', 'crop__50x50')
-        ]
-    )
-    hero = VersatileImageFieldSerializer(
-        sizes=[
-            ('orgnizal_size', 'url'),
-            ('thumbnail', 'thumbnail__1024x250'),
-            # ('square_crop', 'crop__400x400'),
-            # ('small_square_crop', 'crop__50x50')
-        ]
-    )
 
     class Meta:
         model = Product
         fields = (
             'name',
-            'image',
-            'hero'
         )
 
     def create(self, validated_data):
